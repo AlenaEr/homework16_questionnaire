0e02882ae474725011b862794d934dcee56eddf5
alenaer <alana.eremina@gmail.com>
2023-10-31 17:38:06 +0200

    refactoring update styles

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
-let score = 0;
 
-function askMe(question, correctAnswer) {
-    let userAnswer = prompt(question);
-    if (userAnswer === null) {
-        alert('Ок, чекаю ваших питань іншим разом. Гарного дня)')
-        return;
+function cleanAndValidateInput(input) {
+    if (input === null) {
+        alert("You have canceled the input. Please try again.");
+        return null;
     }
-
-    if (userAnswer.trim() === correctAnswer) {
-        alert('Wow! Вірно! 10 балів ваші!');
-        score += 10;
-    } else {
-        alert('Oops! Нажаль ви помилились..');
-    }
+    input = input.trim();
+    input = input.replace(/\s/g, '');
+    return input;
 }
 
-askMe('Скільки буде 2 + 2?', '4');
-askMe('Сонце встає на сході?', 'так');
-askMe('Скільки буде 5 / 0?', 'не можна ділити на 0');
-askMe('Якого кольору небо?', 'синій');
-askMe('Яка правильна відповідь на головне питання життя, всесвіту та всього такого?', '42');
+function capitalize(input) {
+    input =
+        input.charAt(0).toUpperCase() + input.slice(1);
+    return input;
+}
 
-alert(`Гарна гра, ви заробили: ${score} балів`);
+let firstName = prompt('Pls enter your name', 'John');
+firstName = cleanAndValidateInput(firstName);
+firstName = capitalize(firstName);
+
+let lastName = prompt('Pls enter your surname', 'Doe');
+lastName = cleanAndValidateInput(lastName);
+lastName = capitalize(lastName);
+
+let email = prompt('Pls enter your email', 'Ex. john@gmail.com');
+email = cleanAndValidateInput(email);
+
+
+if (!email.includes('@')) {
+    email = `not valid email <b>${email}</b> (symbol @ not exist)`;
+} else if (email.indexOf('@') === 0) {
+    email = `not valid email <b>${email}</b> (symbol @ find in first place)`;
+} else if (email.indexOf('@') === email.length - 1) {
+    email = `not valid email <b>${email}</b> (symbol @ find in last place)`;
+}
+
+let birthYear = prompt('Pls enter your birth year:');
+birthYear = cleanAndValidateInput(birthYear);
+
+let currentYear = new Date().getFullYear();
+let age = currentYear - parseInt(birthYear);
+
+let userFullInfo = document.getElementById('user-info');
+
+let userList = document.createElement('ul');
+userList.innerHTML = `
+  <li>Full name: ${firstName} ${lastName}</li>
+  <li>Email: ${email}</li>
+  <li>Age: ${age}</li>
+`;
+userFullInfo.appendChild(userList);
+
